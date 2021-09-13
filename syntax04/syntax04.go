e08b52f7bf90e26ef7d146538d3bc07b1193dac8
kilhyeonjun <kboxstar@gmail.com>
2021-09-13 22:03:10 +0900

    소수 판정 프로그램 v0.3

--- syntax04/syntax04.go
+++ syntax04/syntax04.go
@@ -6,24 +6,22 @@ import (
 	"time"
 )
 
-// 난수 추출된 수의 소수 판정 프로그램 v0.2
-// 소수 : 1과 자기자신외에는 나누어 떨어지지 않는 수 (0과 1은 제외)
+// 난수 추출된 수의 소수 판정 프로그램 v0.3
 func main() {
 	seed := time.Now().Unix()
 	rand.Seed(seed)
 	
-	count := 0
-	number := rand.Intn(150) + 2 // 0과 1 제외, 2 ~ 151 사이의 수
+	isPrime := true
+	number := rand.Intn(150) + 2 
 	fmt.Println("임의로 추출된 수 : ", number)	
 
-	for i := 2; i < number; i++ { // 1과 number일때 loop 돌지 않음
+	for i := 2; i < number; i++ { 
 		if number % i == 0{
-			// count++
-			count = count + 1
+			isPrime = false
 		}
 	}
 
-	if count == 0 {
+	if isPrime == true {
 		fmt.Println(number ,"는(은) 소수입니다!")
 	}else {
 		fmt.Println(number ,"는(은) 소수가 아닙니다~")
