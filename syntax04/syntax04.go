b963001684b2913a2b61f659be3ba7e59e835d2b
kilhyeonjun <kboxstar@gmail.com>
2021-09-13 22:10:17 +0900

    소수 판정 프로그램 v0.6

--- syntax04/syntax04.go
+++ syntax04/syntax04.go
@@ -6,20 +6,22 @@ import (
 	"time"
 )
 
-// 난수 추출된 수의 소수 판정 프로그램 v0.4
+// 난수 추출된 수의 소수 판정 프로그램 v0.6
 func main() {
 	seed := time.Now().Unix()
 	rand.Seed(seed)
 	
 	isPrime := true
 	number := rand.Intn(150) + 2 
+	// number = 21
 	fmt.Println("임의로 추출된 수 : ", number)	
 
 	for i := 2; i < number; i++ { 
 		if number % i == 0{
 			isPrime = false
+			break // 첫 번째 약수가 발견되면 반복문 즉시 종료
 		}
-		fmt.Print(i, " ")
+		// fmt.Print(i, " ")
 	}
 
 	if isPrime {
