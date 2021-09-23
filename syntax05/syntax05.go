41368381a8bc6ba14022092a090b26171dc1258b
kilhyeonjun <kboxstar@gmail.com>
2021-09-23 21:38:31 +0900

    소수 판정 프로그램 v0.7

---
+++ syntax05/syntax05.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// 입력된 수의 소수 판정 프로그램 v0.7
+func main() {
+	fmt.Print("정수 입력 : ")
+	rd := bufio.NewReader(os.Stdin)
+	in, err := rd.ReadString('\n')
+	
+	if err != nil { // 에러가 발생하면
+		log.Fatal(err)
+	}	
+
+	in = strings.TrimSpace(in)
+	number, err := strconv.Atoi(in)
+	if err != nil { // 에러가 발생하면
+		log.Fatal(err)
+	}
+
+	isPrime := true
+	for i := 2; i < number; i++ { 
+		if number % i == 0{
+			isPrime = false
+			break
+		}
+	}
+	if isPrime {
+		fmt.Println(number ,"는(은) 소수입니다!")
+	}else {
+		fmt.Println(number ,"는(은) 소수가 아닙니다~")
+	}
+}
