555a114a51b6d0cf4c87f3fd677e6053bfd666e0
kilhyeonjun <kboxstar@gmail.com>
2021-09-23 22:07:39 +0900

    shadowing

--- syntax05/syntax05.go
+++ syntax05/syntax05.go
@@ -1,40 +1,60 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"os"
-)
+import "fmt"
 
-// 입력(ftm 패키지의 ScanLn())된 수의 소수 판정 프로그램 v0.9
+// shadowing
 func main() {
-	var number int
+	
+	var test1 float64 = 9.1
+	var test2 float64 = 7.9
+	var univ string = "inha"
 
-	fmt.Print("정수 입력 : ")
-	_, err := fmt.Scanln(&number)
+	var f1 string = "functions"
+	var f2 = append([]string{}, "함수")
 
-	if err != nil{
-		log.Fatal(err)
-	}
-
-	for number < 2{
-		fmt.Println(number ,"는(은) 소수가 아닙니다~")
-		os.Exit(0)
-	}
-
-	isPrime := true
-	for i := 2; i < number; i++ { 
-		if number % i == 0{
-			isPrime = false
-			break
-		}
-	}
-	if isPrime {
-		fmt.Println(number ,"는(은) 소수입니다!")
-	}else {
-		fmt.Println(number ,"는(은) 소수가 아닙니다~")
-	}
+	fmt.Println(test1 + test2)
+	fmt.Println(univ)
+	fmt.Println(f1)
+	fmt.Println(f2)
 }
+
+// package main
+
+// import (
+// 	"fmt"
+// 	"log"
+// 	"os"
+// )
+
+// // 입력(ftm 패키지의 ScanLn())된 수의 소수 판정 프로그램 v0.9
+// func main() {
+// 	var number int
+
+// 	fmt.Print("정수 입력 : ")
+// 	_, err := fmt.Scanln(&number)
+
+// 	if err != nil{
+// 		log.Fatal(err)
+// 	}
+
+// 	for number < 2{
+// 		fmt.Println(number ,"는(은) 소수가 아닙니다~")
+// 		os.Exit(0)
+// 	}
+
+// 	isPrime := true
+// 	for i := 2; i < number; i++ { 
+// 		if number % i == 0{
+// 			isPrime = false
+// 			break
+// 		}
+// 	}
+// 	if isPrime {
+// 		fmt.Println(number ,"는(은) 소수입니다!")
+// 	}else {
+// 		fmt.Println(number ,"는(은) 소수가 아닙니다~")
+// 	}
+// }
 
 // package main
 
